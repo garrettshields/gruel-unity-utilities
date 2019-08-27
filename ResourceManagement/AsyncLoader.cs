c287f9db045c857eefd0df15924d08fff9ef8cc1
garrett <garrettshields@protonmail.com>
2019-08-26 20:26:54 -0400

    * Added LoadResource overload methods to AsyncLoader.

--- ResourceManagement/AsyncLoader.cs
+++ ResourceManagement/AsyncLoader.cs
@@ -7,15 +7,21 @@ namespace Gruel.ResourceManagement {
 	public static class AsyncLoader {
 	
 #region Public Methods
-		public static Coroutine LoadResource(string path, Type type = null, Action<UnityEngine.Object> onLoaded = null, Action<float> onProgress = null) {
-			// Debug.Log($"AsyncLoader.LoadResource: path: {path}");
+		public static Coroutine LoadResource(string path, Action<UnityEngine.Object> onLoaded) {
+			return CoroutineRunner.StartCoroutine(AsyncLoaderCor(path, null, onLoaded, null));
+		}
 		
+		public static Coroutine LoadResource(string path, Action<UnityEngine.Object> onLoaded, Action<float> onProgress) {
+			return CoroutineRunner.StartCoroutine(AsyncLoaderCor(path, null, onLoaded, onProgress));
+		}
+
+		public static Coroutine LoadResource(string path, Type type = null, Action<UnityEngine.Object> onLoaded = null, Action<float> onProgress = null) {
 			return CoroutineRunner.StartCoroutine(AsyncLoaderCor(path, type, onLoaded, onProgress));
 		}
 #endregion Public Methods
 
 #region Private Methods
-		private static IEnumerator AsyncLoaderCor(string path, Type type = null, Action<UnityEngine.Object> onLoaded = null, Action<float> onProgress = null) {
+		private static IEnumerator AsyncLoaderCor(string path, Type type, Action<UnityEngine.Object> onLoaded, Action<float> onProgress) {
 			var asyncOperation = Resources.LoadAsync(path, type == null ? typeof(UnityEngine.Object) : type);
 
 			while (asyncOperation.isDone == false) {
